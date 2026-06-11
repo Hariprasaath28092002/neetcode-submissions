faa48060a770e63f4dabe98bdbd52e55733df74e
Hariprasaath S <84852101+Hariprasaath28092002@users.noreply.github.com>
2026-06-11 11:37:10 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        for word in strs:
+            sorted_word = "".join(sorted(word)) #here after sorting  it will give list of character.but we want as string so .join used
+
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+
+        return list(anagrams.values())
+
+
+        # anagram_map = defaultdict(list)
+        # for word in strs:
+        #     count = [0] * 26
+
+        #     for ch in word:
+        #         count[ord(ch) - ord('a')] += 1
+
+        #     key = tuple(count)
+        #     anagram_map[key].append(word)
+
+        # return list(anagram_map.values())    
