ebcb019dfc4d47f5647fe953a9e5ddceea2dba0d
Hariprasaath S <84852101+Hariprasaath28092002@users.noreply.github.com>
2026-06-11 11:20:03 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+
+            key = tuple(count)
+            anagram_map[key].append(word)
+
+        return list(anagram_map.values())    
